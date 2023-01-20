3f91d750a90d9e86fed1f2da4b3fe3a70b56b867
y1s1285312 <70126240+y1s1285312@users.noreply.github.com>
2023-01-20 22:09:45 +0900

    Update kisaAlarm.py

--- kisaAlarm.py
+++ kisaAlarm.py
@@ -3,17 +3,10 @@ from bs4 import BeautifulSoup
 import datetime
 import threading
 import requests
-from twilio.rest import Client
-import os
-
-#account_sid = os.environ.get('ackey')
-#auth_token = os.environ.get('authkey')
-#client = Client(account_sid, auth_token)
-
-
+import atexit
 
 period = 300
-removeFindedPeriod=6
+removeFindedPeriod = 0
 
 finded = []
 
@@ -23,22 +16,23 @@ def removeFindedData():
     today = datetime.datetime.now().strftime('%Y.%m.%d')
 
     d1 = today.split('.')
-    d1 = datetime.date(int(d1[0]),int(d1[1]),int(d1[2]))
+    d1 = datetime.date(int(d1[0]), int(d1[1]), int(d1[2]))
     dellist = []
 
-    for i,x in enumerate(finded):
+    for i, x in enumerate(finded):
         d2 = x.split('/')[0].split('.')
-        d2 = datetime.date(int(d2[0]),int(d2[1]),int(d2[2]))
-        if (d1-d2).days > removeFindedPeriod:
+        d2 = datetime.date(int(d2[0]), int(d2[1]), int(d2[2]))
+        if (d1 - d2).days > removeFindedPeriod:
             dellist.append(x)
 
     for x in dellist:
+        print('delData',x)
         finded.remove(x)
-
 
 
 def startAlarm():
     print('startAlarm')
+    msg='새 보안권고문\n'
     global finded
     today = datetime.datetime.now().strftime('%Y.%m.%d')
     d1 = today.split('.')
@@ -57,36 +51,26 @@ def startAlarm():
         title = data.find('td', attrs={'class': 'colTit'}).text.strip('\n')
         d2 = time.split('.')
         d2 = datetime.date(int(d2[0]), int(d2[1]), int(d2[2]))
-        chk = len(list(filter(lambda x : title in x,finded)))
-        
-        if (d1-d2).days ==0 and chk==0:
-            str = '{}/{}'.format(time,title)
+        chk = len(list(filter(lambda x: title in x, finded)))
+
+        if (d1 - d2).days == 0 and chk == 0:
+            str = '{}/{}'.format(time, title)
             res.append(str)
+            msg += title+'\n'
             finded.append(str)
 
-    if len(res)>0:
+    if len(res) > 0:
         print('send msg')
-        
-        response = requests.get("https://api.telegram.org/bot5842805214:AAEogW_ZtELsS4zVMvOBfI_jPfvHWIobtNc/sendMessage?chat_id=5894259370&text=새 보안권고문")
-        
-        '''
-        client.messages \
-            .create(
-            body="새 보안권고문",
-            from_='+19107086825',
-            to='+821053429022'
-        )
-        '''
+        print(res)
+        print(finded)
+        requests.get("https://api.telegram.org/bot5842805214:AAEogW_ZtELsS4zVMvOBfI_jPfvHWIobtNc/sendMessage?chat_id=5894259370&text={}".format(msg))
 
-
-
-    print(res)
-    print(finded)
+    
     removeFindedData()
     timer.start()
 
+def handle_exit():
+    print('종료')
 
-
-
-
+atexit.register(handle_exit)
 startAlarm()
