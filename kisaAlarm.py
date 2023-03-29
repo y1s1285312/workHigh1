d12bd11c7b9dd1506481e67fdd60331fbf46f56b
y1s1285312 <70126240+y1s1285312@users.noreply.github.com>
2023-03-29 12:54:08 +0900

    Update kisaAlarm.py

--- kisaAlarm.py
+++ kisaAlarm.py
@@ -4,6 +4,8 @@ import datetime
 import threading
 import requests
 import atexit
+import smtplib
+from email.mime.text import MIMEText
 
 period = 300
 removeFindedPeriod = 0
@@ -30,9 +32,49 @@ def removeFindedData():
         finded.remove(x)
 
 
+def makeMailMsg(title,time,link):
+    msg='''
+    <header style="margin: 0px; padding: 0px; 
+    color: rgb(34, 34, 34); box-sizing: border-box; text-size-adjust: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); 
+    -webkit-font-smoothing: antialiased; position: relative; border-top: 2px solid rgb(85, 85, 85); font-family: 
+    &quot;Noto Sans KR&quot;, Roboto, &quot;Apple SD Gothic Neo&quot;, sans-serif; font-size: 16px; letter-spacing: -0.8px;">
+    <div class="b_title" style="margin: 0px; padding: 0px 20px; color: inherit; box-sizing: border-box; text-size-adjust: none;
+     -webkit-tap-highlight-color: rgba(0, 0, 0, 0); -webkit-font-smoothing: antialiased; display: inline-block; min-height: 45px; 
+     border-style: solid; border-color: rgba(0, 0, 0, 0.15); border-image: initial; border-width: 0px 0px 1px; 
+     background: rgb(237, 240, 245); width: 1183.2px;"><h2 style="margin: 0px; padding: 20px 0px; color: 
+     inherit; box-sizing: border-box; text-size-adjust: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); 
+     -webkit-font-smoothing: antialiased; position: relative; clear: both; font-size: 17.6px; min-height: 50px;
+      float: left; word-break: break-all;">{}</h2><span style="margin: 0px; padding: 20px 0px; 
+      color: inherit; box-sizing: border-box; text-size-adjust: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); 
+      -webkit-font-smoothing: antialiased; display: inline-block; min-height: 50px; float: right;">{}</span></div></header>
+      '''.format(title,time)
+    url = 'https://www.krcert.or.kr'+link
+    html = urllib.request.urlopen(url).read()
+    res = BeautifulSoup(html, 'html.parser')
+    datas = str(res.select('#View > div.board > div > div.content_html > table')[0])
+
+    return msg+datas
+
+def sendmail(mailmsg):
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+
+    smtp.ehlo()
+
+    smtp.starttls()
+
+    smtp.login('y1s1285312@gmail.com', 'aojnpoycjplroaqw')
+
+    msg = MIMEText(mailmsg)
+    msg['Subject'] = '보안권고문'
+
+    smtp.sendmail('y1s1285312@gmail.com', 'socmm05@high1.com', msg.as_string())
+
+    smtp.quit()
+
 def startAlarm():
     print('startAlarm')
     msg = ''
+    mailmsg=''
     global finded
     today = datetime.datetime.now().strftime('%Y.%m.%d')
     d1 = today.split('.')
@@ -51,6 +93,8 @@ def startAlarm():
         #print(time)
         title = data.find('td', attrs={'class': 'sbj tal'}).text.strip('\n').strip()
         #print(title)
+        link = data.find('td', attrs={'class': 'sbj tal'}).find('a')['href']
+        #print(link)
         d2 = time.split('-')
         d2 = datetime.date(int(d2[0]), int(d2[1]), int(d2[2]))
         chk = len(list(filter(lambda x: title in x, finded)))
@@ -59,18 +103,23 @@ def startAlarm():
             str = '{}/{}'.format(time, title)
             res.append(str)
             msg += title + '\n'
+            mailmsg += makeMailMsg(title,time,link)
+
             finded.append(str)
 
     #print('res',res)
 
-
+    
     if len(res) > 0:
         print('send msg')
         print('res',res)
         print('finded',finded)
+        sendmail(mailmsg)
+        
         requests.get(
             "https://api.telegram.org/bot5842805214:AAEogW_ZtELsS4zVMvOBfI_jPfvHWIobtNc/sendMessage?chat_id=-1001258042021&text={}".format(
                 msg))
+    
 
     removeFindedData()
     timer.start()
